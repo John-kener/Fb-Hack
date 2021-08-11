13eaf7970b6588d9885bd54567f15ff4daa01852
John Kener <73270248+John-kener@users.noreply.github.com>
2021-08-11 11:51:24 +0530

    Add files via upload

---
+++ requirements.rb
@@ -0,0 +1,16 @@
+system("clear")
+puts("\e[1;31m\n INSTALLING REQUIREMENTS ...")
+puts ("\033[0m ")
+system("termux-setup-storage")
+system("pkg update && pkg upgrade")
+system("pkg install ruby -y")
+system("pkg install figlet -y")
+system("pkg install python -y")
+system("gem install lolcat")
+system("pkg install nano -y")
+system("clear")
+puts(" ")
+puts ("\033[32m INSTALLATION COMPLETED")
+puts ("\033[0m ")
+puts "\n\n [+]\033[32m Now Run :- \033[94m ruby fb-hack.rb\n\n"
+
